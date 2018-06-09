c214caa63bfae1026626110aeec38a242eaaa028
Kohki Sato <you@example.com>
2018-06-09 12:50:49 +0900

    I added news articels function from NY times

--- js/script.js
+++ js/script.js
@@ -11,16 +11,39 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    // load streetview
-    var street = $("#street").val();
-    var city = $("#city").val();
-    var address = street + + ', ' + city
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
 
-    $greeting.text('So, you are planning to live around ' + city + street + '?' )
-    streeviewurl = "http://maps.googleapis.com/maps/api/streetview?size=600x300&location=" + address + '';
+    $greeting.text('So, you want to live at ' + address + '?');
+
+
+    // load streetview
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+
+    // load nytimes
+    var nytimesurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    nytimesurl += '?' + $.param({
+      'api-key': "a280a205aa0144cdb9b52bae2b7c7212",
+      'q': "test"
+    });
 
     // YOUR CODE GOES HERE!
-    $body.append($body.append('<img class="bgimg" src="' + streeviewurl + '"">'));
+    $.getJSON( nytimesurl, function( data ) {
+      var items = [];
+      // console.log(data)
+      // console.log(data.response.docs[0].web_url)
+
+      articles = data.response.docs
+
+      for (var i = 0; i < articles.length; i++) {
+          var article = articles[i];
+          $nytElem.append('<li class="article">' + '<a href="' + article.web_url + '">' + article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + '</li>');
+      };
+    }); 
+
 
     return false;
 };
