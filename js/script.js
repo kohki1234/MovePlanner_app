7bb23db97cf588f927f6fb5710d3a671324853cb
Kohki Sato <you@example.com>
2018-06-09 18:43:58 +0900

    I implemented the js file to have wikipedia article search.

--- js/script.js
+++ js/script.js
@@ -32,8 +32,6 @@ function loadData() {
 
     // YOUR CODE GOES HERE!
     $.getJSON( nytimesurl, function( data ) {
-
-      var items = [];
       // console.log(data)
       // console.log(data.response.docs[0].web_url)
 
@@ -48,6 +46,48 @@ function loadData() {
         $nytElem.append('<li class="article">' + 'New York times data was failed to be loaded due to some reason..., please try again later' + '</li>')
       });  
 
+
+    // load wikipedia articles
+    var wikiurl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + address.replace(', ', '%20') + '&format=json';
+
+    console.log(wikiurl)
+
+    // CODE TO LOAD the data
+    $.ajax({
+      url: wikiurl,
+      jsonp: "callback",
+      dataType: 'jsonp',   
+      data: {
+        q: "select title,abstract,url from search.news where query=\"cat\"",
+        format: "json"
+    },
+      success:function (result){
+        // test to verify which string I got, and sucess of JSONPrequest.
+
+        // console.log("GOOD");
+        // console.log(result[0]);
+        // console.log(result[1]); 
+        // console.log(result[2]);
+
+        wikiurldescription = result[1];
+        wikiarticleurl = result[3];
+
+
+        for (var i = 0; i < result.length; i++) {
+            finaldescription = wikiurldescription[i];
+            // console.log(finaldescription);
+
+            // $wikiElem.append('<li id="wikipedia-links">' + finaldescription + '</li>');
+
+
+            finalurl =wikiarticleurl[i];
+            // console.log(finalurl);
+            $wikiElem.append('<li id="wikipedia-links">' + '<a href="' + finalurl + '"">' + finaldescription + '</a>' + '</li>')
+            console.log('<li id="wikipedia-links">' + '<a href="' + finalurl + '"">' + finaldescription + '</a>' + '</li>')
+
+        }
+      }
+    });
 
     return false;
 };
